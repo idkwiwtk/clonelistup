3a179bb5d92c271aaa108652dda7700924c66074
idkwiwtk <bh5619@naver.com>
2022-10-28 16:50:24 +0900

    back up

--- src/js/guide.horizen.scroll.js
+++ src/js/guide.horizen.scroll.js
@@ -1,9 +1,35 @@
 var PREV_WINDOW_Y = $(window).scrollTop();
 
+const BEGIN = "begin";
+const END = "end";
+
+var scrollBlock = false;
+var guideSwiperReach = BEGIN;
+
 const UP = "up";
 const DOWN = "down";
+const SCROLL_STEP = 20;
 
-function guideScroll(event, el, step) {
+/**
+ * 가이드 섹션 슬라이드 객체 선언
+ */
+var guideSwiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  slidesperView: "auto",
+  mousewheel: true,
+  on: {
+    reachBeginning: function () {
+      guideSwiperReach = BEGIN;
+      guideSwiper.mousewheel.disable();
+    },
+    reachEnd: function () {
+      guideSwiperReach = END;
+      guideSwiper.mousewheel.disable();
+    },
+  },
+});
+
+function guideScroll(el) {
   let windowY = $(window).scrollTop();
   let elOffTop = $(el).offset().top;
   let elContentWidth = $(el).outerWidth();
@@ -18,19 +44,90 @@ function guideScroll(event, el, step) {
     mode = UP;
   }
 
-  console.log("Window y", windowY);
-
   PREV_WINDOW_Y = windowY;
 
-  if (elOffTop >= windowY && elX != 0 && mode == UP) {
+  if (elOffTop <= windowY) {
+    setGlobalScroll(true);
     $(window).scrollTop(elOffTop);
-    $(el).scrollLeft(elX - step);
-  } else if (elOffTop <= windowY) {
-    if (elX >= elContentWidth) {
-      $(el).scrollLeft(elContentWidth);
-    } else {
-      $(window).scrollTop(elOffTop);
-      $(el).scrollLeft(elX + step);
-    }
   }
+
+  // if (elOffTop >= windowY && elX != 0 && mode == UP) {
+  //   event.preventDefault();
+  //   event.stopPropagation();
+  //   $(window).scrollTop(elOffTop);
+  //   $(el).scrollLeft(elX - step);
+  // } else if (elOffTop <= windowY) {
+  //   if (elX >= elContentWidth) {
+  //     $(el).scrollLeft(elContentWidth);
+  //   } else {
+  //     $(window).scrollTop(elOffTop);
+  //     $(el).scrollLeft(elX + step);
+  //   }
+  // }
+}
+
+/**
+ *
+ * @param {global scroll control} block
+ */
+function setGlobalScroll(block) {
+  scrollBlock = block;
+}
+
+function startGuideScrollEvent() {
+  $(window).scroll(function (e) {
+    guideScroll("#guide");
+  });
+}
+
+function startGuideWheelEvent(el, nextEl) {
+  document.addEventListener(
+    "wheel",
+    function (e) {
+      let windowY = $(window).scrollTop();
+      let elOffTop = $(el).offset().top;
+      let nextElOffTop = $(nextEl).offset().top;
+
+      if (e.deltaY < 0) {
+        // scroll up
+        console.log("scroll up");
+
+        if (elOffTop > windowY) {
+          setGlobalScroll(true);
+          $(window).scrollTop(elOffTop);
+        }
+      } else {
+        // scroll down
+        console.log("scroll down");
+
+        // 가이드 섹션 영역
+        if (
+          elOffTop < windowY &&
+          guideSwiperReach != END &&
+          nextElOffTop > windowY
+        ) {
+          console.log();
+          setGlobalScroll(true);
+          $(window).scrollTop(elOffTop);
+        } else {
+          setGlobalScroll(false);
+          guideSwiper.mousewheel.enable();
+        }
+      }
+      if (scrollBlock) {
+        e.preventDefault();
+        e.stopPropagation();
+      } else {
+      }
+    },
+    { passive: false }
+  );
+}
+
+function startGuideSwiper() {}
+
+function guideInit() {
+  // startGuideScrollEvent();
+  startGuideWheelEvent("#guide", "#service");
+  startGuideSwiper();
 }
--- src/js/init.js
+++ src/js/init.js
@@ -1,20 +1,11 @@
 const animationTextList = ["Archiving", "Tagging", "Trading"];
 
-const SCROLL_STEP = 7;
-
 $(document).ready(function () {
-  // $("#smooth-wrapper").smoothWheel();
   typing($(".text-animation h2"), animationTextList, 300);
 });
 
-let chk = true;
-$(window).scroll(function (e) {
-  if (chk) {
-    console.log(e.target.wheelDelta);
-    chk = false;
-    setTimeout(function () {
-      chk = true;
-    }, 1);
-    guideScroll(e, $("#guide"), SCROLL_STEP);
-  }
-});
+// 가이드 섹션 가로 스크롤 초기화
+guideInit();
+
+// jquery smooth scroll
+$("html").easeScroll();
