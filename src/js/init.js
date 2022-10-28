ee4331f2d6ee3b6f643006605dce50bfbafdd4bf
idkwiwtk <bh5619@naver.com>
2022-10-28 18:55:20 +0900

    clean up code

--- src/js/init.js
+++ src/js/init.js
@@ -3,9 +3,3 @@ const animationTextList = ["Archiving", "Tagging", "Trading"];
 $(document).ready(function () {
   typing($(".text-animation h2"), animationTextList, 300);
 });
-
-// 가이드 섹션 가로 스크롤 초기화
-guideInit();
-
-// jquery smooth scroll
-$("html").easeScroll();
