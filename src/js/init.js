f9836eb2cd8f82b711383e53b8284161287c8b8c
idkwiwtk <bh5619@naver.com>
2022-10-28 19:39:53 +0900

    horizontal scrolling

--- src/js/init.js
+++ src/js/init.js
@@ -3,3 +3,5 @@ const animationTextList = ["Archiving", "Tagging", "Trading"];
 $(document).ready(function () {
   typing($(".text-animation h2"), animationTextList, 300);
 });
+
+$("html").easeScroll();
