ac8cc4b12bc554a98fae5ef8348c5b88cbba4fce
idkwiwtk <bh5619@naver.com>
2022-10-28 17:16:50 +0900

    backup last

--- src/js/init.js
+++ src/js/init.js
@@ -5,7 +5,59 @@ $(document).ready(function () {
 });
 
 // 가이드 섹션 가로 스크롤 초기화
-guideInit();
+// guideInit();
 
 // jquery smooth scroll
 $("html").easeScroll();
+
+// swiper
+
+var swiperTop = new Swiper(".swiper-top", {
+  direction: "vertical",
+  // slidesperView: "auto",
+  mousewheel: true,
+  speed: 500,
+  effect: "coverflow",
+  // on: {
+  //   reachBeginning: function () {
+  //     guideSwiperReach = BEGIN;
+  //     guideSwiper.mousewheel.disable();
+  //   },
+  //   reachEnd: function () {
+  //     guideSwiperReach = END;
+  //     guideSwiper.mousewheel.disable();
+  //   },
+  // },
+});
+
+var swiperMiddle = new Swiper(".swiper-middle", {
+  direction: "horizontal",
+  slidesperView: "auto",
+  mousewheel: true,
+  // on: {
+  //   reachBeginning: function () {
+  //     guideSwiperReach = BEGIN;
+  //     guideSwiper.mousewheel.disable();
+  //   },
+  //   reachEnd: function () {
+  //     guideSwiperReach = END;
+  //     guideSwiper.mousewheel.disable();
+  //   },
+  // },
+});
+
+var swiperBottom = new Swiper(".swiper-bottom", {
+  direction: "vertical",
+  slidesperView: "auto",
+  mousewheel: true,
+  // on: {
+  //   reachBeginning: function () {
+  //     guideSwiperReach = BEGIN;
+  //     guideSwiper.mousewheel.disable();
+  //   },
+  //   reachEnd: function () {
+  //     guideSwiperReach = END;
+  //     guideSwiper.mousewheel.disable();
+  //   },
+  // },
+});
