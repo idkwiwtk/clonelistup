f27b1233c1b4700c978c6ca651e8ad8483309d33
idkwiwtk <bh5619@naver.com>
2022-10-28 18:35:26 +0900

    swiper slide example

--- src/js/init.js
+++ src/js/init.js
@@ -1,63 +1,134 @@
 const animationTextList = ["Archiving", "Tagging", "Trading"];
 
 $(document).ready(function () {
-  typing($(".text-animation h2"), animationTextList, 300);
+  // typing($(".text-animation h2"), animationTextList, 300);
 });
 
-// 가이드 섹션 가로 스크롤 초기화
-// guideInit();
-
-// jquery smooth scroll
-$("html").easeScroll();
-
 // swiper
-
 var swiperTop = new Swiper(".swiper-top", {
   direction: "vertical",
-  // slidesperView: "auto",
   mousewheel: true,
-  speed: 500,
-  effect: "coverflow",
-  // on: {
-  //   reachBeginning: function () {
-  //     guideSwiperReach = BEGIN;
-  //     guideSwiper.mousewheel.disable();
-  //   },
-  //   reachEnd: function () {
-  //     guideSwiperReach = END;
-  //     guideSwiper.mousewheel.disable();
-  //   },
-  // },
+  on: {
+    slideChangeTransitionEnd: function () {
+      if (swiperTop.isBeginning) {
+        console.log("reachBeginning: top");
+        setTimeout(function () {
+          swiperTop.mousewheel.enable();
+          swiperMiddle.mousewheel.disable();
+          swiperBottom.mousewheel.disable();
+        }, 100);
+      } else if (swiperTop.isEnd) {
+        console.log("reachEnd: top");
+        setTimeout(function () {
+          swiperTop.mousewheel.disable();
+          // swiperTop.mousewheel.enable();
+          swiperMiddle.mousewheel.enable();
+          swiperBottom.mousewheel.disable();
+        }, 100);
+      }
+    },
+    // reachBeginning: function () {
+    //   console.log("reachBeginning: top");
+    //   setTimeout(function () {
+    //     swiperTop.mousewheel.enable();
+    //     swiperMiddle.mousewheel.disable();
+    //     swiperBottom.mousewheel.disable();
+    //   }, 1000);
+    // },
+    // reachEnd: function () {
+    //   console.log("reachEnd: top");
+    //   setTimeout(function () {
+    //     swiperTop.mousewheel.disable();
+    //     swiperMiddle.mousewheel.enable();
+    //     swiperBottom.mousewheel.disable();
+    //   }, 1000);
+    // },
+  },
 });
 
 var swiperMiddle = new Swiper(".swiper-middle", {
   direction: "horizontal",
-  slidesperView: "auto",
   mousewheel: true,
-  // on: {
-  //   reachBeginning: function () {
-  //     guideSwiperReach = BEGIN;
-  //     guideSwiper.mousewheel.disable();
-  //   },
-  //   reachEnd: function () {
-  //     guideSwiperReach = END;
-  //     guideSwiper.mousewheel.disable();
-  //   },
-  // },
+  on: {
+    slideChange: function () {
+      console.log("change");
+    },
+    slideChangeTransitionEnd: function () {
+      if (swiperMiddle.isBeginning) {
+        console.log("reachBeginning: Middle");
+        setTimeout(function () {
+          swiperTop.mousewheel.enable();
+          swiperMiddle.mousewheel.disable();
+          // swiperMiddle.mousewheel.enable();
+          swiperBottom.mousewheel.disable();
+        }, 100);
+      } else if (swiperMiddle.isEnd) {
+        console.log("reachEnd: Middle");
+        setTimeout(function () {
+          swiperTop.mousewheel.disable();
+          swiperMiddle.mousewheel.disable();
+          // swiperMiddle.mousewheel.enable();
+          swiperBottom.mousewheel.enable();
+        }, 100);
+      }
+    },
+    // reachBeginning: function () {
+    //   console.log("reachBeginning: Middle");
+    //   setTimeout(function () {
+    //     swiperTop.mousewheel.enable();
+    //     swiperMiddle.mousewheel.disable();
+    //     swiperBottom.mousewheel.disable();
+    //   }, 1000);
+    // },
+    // reachEnd: function () {
+    //   console.log("reachEnd: Middle");
+    //   setTimeout(function () {
+    //     swiperTop.mousewheel.disable();
+    //     swiperMiddle.mousewheel.disable();
+    //     swiperBottom.mousewheel.enable();
+    //   }, 1000);
+    // },
+  },
 });
 
 var swiperBottom = new Swiper(".swiper-bottom", {
   direction: "vertical",
-  slidesperView: "auto",
   mousewheel: true,
-  // on: {
-  //   reachBeginning: function () {
-  //     guideSwiperReach = BEGIN;
-  //     guideSwiper.mousewheel.disable();
-  //   },
-  //   reachEnd: function () {
-  //     guideSwiperReach = END;
-  //     guideSwiper.mousewheel.disable();
-  //   },
-  // },
+  on: {
+    slideChangeTransitionEnd: function () {
+      if (swiperBottom.isBeginning) {
+        console.log("reachBeginning: Bottom");
+        setTimeout(function () {
+          swiperTop.mousewheel.disable();
+          swiperMiddle.mousewheel.enable();
+          swiperBottom.mousewheel.disable();
+          // swiperBottom.mousewheel.enable();
+        }, 100);
+      } else if (swiperBottom.isEnd) {
+        console.log("reachEnd: Bottom");
+        setTimeout(function () {
+          swiperTop.mousewheel.disable();
+          swiperMiddle.mousewheel.disable();
+          // swiperMiddle.mousewheel.enable();
+          swiperBottom.mousewheel.enable();
+        }, 100);
+      }
+    },
+    // reachBeginning: function () {
+    //   console.log("reachBeginning: Bottom");
+    //   setTimeout(function () {
+    //     swiperTop.mousewheel.disable();
+    //     swiperMiddle.mousewheel.enable();
+    //     swiperBottom.mousewheel.disable();
+    //   }, 1000);
+    // },
+    // reachEnd: function () {
+    //   console.log("reachEnd: Bottom");
+    //   setTimeout(function () {
+    //     swiperTop.mousewheel.disable();
+    //     swiperMiddle.mousewheel.disable();
+    //     swiperBottom.mousewheel.enable();
+    //   }, 1000);
+    // },
+  },
 });
