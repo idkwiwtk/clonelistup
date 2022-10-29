4f60e53eb1c97befeaa2fada0864a3aff4cdeae3
idkwiwtk <bh5619@naver.com>
2022-10-29 14:48:50 +0900

    section guide finish

---
+++ src/js/guide.intro.swiper.js
@@ -0,0 +1,39 @@
+const swiperIntro01 = new Swiper(".swiper-intro-01", {
+  direction: "horizontal",
+  effect: "fade",
+  pagination: { el: ".swiper-intro-01 .swiper-pagination" },
+  navigation: {
+    prevEl: ".swiper-intro-01 .swiper-button-prev",
+    nextEl: ".swiper-intro-01 .swiper-button-next",
+  },
+});
+
+const swiperIntro02 = new Swiper(".swiper-intro-02", {
+  direction: "horizontal",
+  effect: "fade",
+  pagination: { el: ".swiper-intro-02 .swiper-pagination" },
+  navigation: {
+    prevEl: ".swiper-intro-02 .swiper-button-prev",
+    nextEl: ".swiper-intro-02 .swiper-button-next",
+  },
+});
+
+const swiperIntro03 = new Swiper(".swiper-intro-03", {
+  direction: "horizontal",
+  effect: "fade",
+  pagination: { el: ".swiper-intro-03 .swiper-pagination" },
+  navigation: {
+    prevEl: ".swiper-intro-03 .swiper-button-prev",
+    nextEl: ".swiper-intro-03 .swiper-button-next",
+  },
+});
+
+const swiperIntro04 = new Swiper(".swiper-intro-04", {
+  direction: "horizontal",
+  effect: "fade",
+  pagination: { el: ".swiper-intro-04 .swiper-pagination" },
+  navigation: {
+    prevEl: ".swiper-intro-04 .swiper-button-prev",
+    nextEl: ".swiper-intro-04 .swiper-button-next",
+  },
+});
